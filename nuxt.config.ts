c9a089bfff2355e65d7674146b0e2131399191ca
s-a-tanjim <shoebtanjim@outlook.com>
2023-01-30 20:00:06 +0600

    aaa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  router: {
-    base: "/page_test/"
-  },
   app: {
     baseURL: '/page_test/',
     head: {
